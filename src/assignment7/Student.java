3783ef0f21be8bf79014edda84c494da0eea7d66
morgan <morgan@172.27.38.146>
2022-11-28 15:32:16 -0600

    Finished #3

--- src/assignment7/Student.java
+++ src/assignment7/Student.java
@@ -1,24 +1,166 @@
 package assignment7;
 
-//TODO: Copy a working Student class from Assignment 7 over this file.
 public class Student {
-	public Student(String firstName, String lastName, int id) {
-		throw new RuntimeException("TODO: Copy a working Student class from Assignment 7 over this file.");
-	}
 
+	private String firstName = "";// First Name
+	private String lastName = "";// Last Name
+	private int sID = 0;// Student ID Number
+	private int att = 0;// Attempted Credits
+	private int pass = 0;// Passing Credits
+	private double gqp = 0; // Total Grade Quality Points
+	private double bb = 0.0;// Bear Bucks Balance
+	/*
+	 * Constructs a student without a bb count
+	 * @param firstName is the students firstname
+	 * @param lastName is the students lastname
+	 * @param myInt is the students ID
+	 */
+	public Student(String firstName, String lastName, int myInt) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.sID = myInt;
+	}
+	/*
+	 * Constructs a student with a bb count
+	 * @param firstName is the students firstname
+	 * @param lastName is the students lastname
+	 * @param myInt is the students ID
+	 * @param bb is the students bear bucks count
+	 */
+	public Student(String firstName, String lastName, int myInt, double bb) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.sID = myInt;
+		this.bb = bb;
+	}
+	/*
+	 * Returns the students full name
+	 */
 	public String getFullName() {
-		throw new RuntimeException("TODO: Copy a working Student class from Assignment 7 over this file.");
+		return firstName + " " + lastName;
 	}
-
+	/*
+	 * Returns the students ID
+	 */
 	public int getId() {
-		throw new RuntimeException("TODO: Copy a working Student class from Assignment 7 over this file.");
+		return sID;
+	}
+	/*
+	 * adds a grade to the student's gpa.
+	 * @param grade is the grade in the course
+	 * @param credit is the credit that class is worth
+	 */
+	public void submitGrade(double grade, int credit) {
+		att += credit;
+		if (grade >= 1.7) {
+			pass += credit;
+		}
+		gqp += credit * grade;
 	}
 
+	/*
+	 * Returns the credits the student has attempted
+	 */
+	public int getTotalAttemptedCredits() {
+		return att;
+	}
+	/*
+	 * Returns the credits the student has passed
+	 */
+	public int getTotalPassingCredits() {
+		return pass;
+	}
+	/*
+	 * Sets the student's firstname
+	 * @param fn is the student's new firstname
+	 */
+	public void setFirstName(String fn) {
+		firstName = fn;
+	}
+
+	/*
+	 * Caclulates the students GPA
+	 */
+	public double calculateGradePointAverage() {
+		return gqp / att; 
+	}
+	/*
+	 * Returns a string with the student's class standing
+	 */
+	public String getClassStanding() {
+		if (pass < 30) {
+			return "First Year";
+		} else if (pass < 60) {
+			return "Sophomore";
+		} else if (pass < 90) {
+			return "Junior";
+		} else {
+			return "Senior";
+		}
+	}
+	/*
+	 * Returns boolean if student is eligible for PBK
+	 */
+	public boolean isEligibleForPhiBetaKappa() {
+		if (pass >= 98 && gqp / att >= 3.6 || pass >= 75 && gqp / att >= 3.8) {
+			return true;
+		}
+		return false;
+	}
+	/*
+	 * deposits amount bear bucks into the student's account
+	 * @param amount is the amount of BB depoisited
+	 */
 	public void depositBearBucks(double amount) {
-		throw new RuntimeException("TODO: Copy a working Student class from Assignment 7 over this file.");
+		bb += amount;
 	}
-
+	/*
+	 * deducts amount bear bucks into the student's account
+	 * @param amount is the amount of BB deducted
+	 */
+	public void deductBearBucks(double amount) {
+		bb -= amount;
+	}
+	/*
+	 * returns the bearbucks in the students account
+	 */
 	public double getBearBucksBalance() {
-		throw new RuntimeException("TODO: Copy a working Student class from Assignment 7 over this file.");
+		return bb;
+	}
+	/*
+	 * cashes out bear bucks from the students account, with a 10$ fee.
+	 */
+	public double cashOutBearBucks() {
+		double temp = bb;
+		bb = 0;
+		if (temp <= 10) {
+			return 0;
+		} else {
+			return temp - 10;
+		}
+	}
+	/*
+	 * Creates a legacy student from two parents, dropping down those parent's bearbucks balances.
+	 * @param firstName is the students firstname
+	 * @param other is the other parent to drop balance from
+	 * @param isHyphenated is if the lastname is hyphenated both parents' names.
+	 * @param id is the student's new id.
+	 */
+	public Student createLegacy(String firstName, Student other, boolean isHyphenated, int id) {
+		int p1ID = this.sID;
+		String p1lastName = this.lastName;
+		if (isHyphenated) {
+			return new Student(firstName, p1lastName + "-" + other.lastName, id,
+					this.cashOutBearBucks() + other.cashOutBearBucks());
+		}
+		return new Student(firstName, p1lastName, id, this.cashOutBearBucks() + other.cashOutBearBucks());
+	}
+	/*
+	 * Returns the student name and ID.
+	 */
+	public String toString() {
+		return getFullName() + " " + getId();
 	}
 }
--- src/studio9/UniversityDatabase.java
+++ src/studio9/UniversityDatabase.java
@@ -1,26 +1,46 @@
 package studio9;
 
+import java.util.Map;
+import java.util.HashMap;
+
+
 import assignment7.Student;
 
 public class UniversityDatabase {
 	//TODO: Complete this class according to the studio instructions
+	private final Map<String, Student> myMap;
 
+
+	public UniversityDatabase() {
+		this.myMap = new HashMap<String, Student>();
+
+	}
+	
 	public void addStudent(String accountName, Student student) {
-		// TODO
+		this.myMap.put(accountName, student);
 	}
 
 	public int getStudentCount() {
 		// TODO
-		return 0;
+		return this.myMap.size();
 	}
 
 	public String lookupFullName(String accountName) {
 		// TODO: Complete according to studio instructions
-		return null;
+		if(accountName == null || this.myMap.get(accountName) == null) {
+			return null;
+		}
+		else {
+			return this.myMap.get(accountName).getFullName();
+		}
 	}
 
 	public double getTotalBearBucks() {
 		// TODO
-		return 0.0;
+		double total = 0.0;
+		for(Map.Entry<String, Student> s : myMap.entrySet()) {
+			total+=s.getValue().getBearBucksBalance();
+		}
+		return total;		
 	}
 }
